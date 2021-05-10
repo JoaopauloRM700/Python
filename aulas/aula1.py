a9d44a7a6518fda65c3ccac83a181559be4fbb73
Joao Paulo Reis Marques <44990001+JoaopauloRM700@users.noreply.github.com>
2021-05-10 08:56:07 -0400

    py

---
+++ aulas/aula1.py
@@ -0,0 +1,12 @@
+#print("Oi mundo") # aula 1 
+# O terminador de comando do python são os Enter's/quebra de linha
+# entretanto se quiser mais de um comando na mesma linha, devesse
+# finalizar os comandos com ';'
+
+# Variaveis Globais são declaradas fora das funções ou sendo antecedidas da
+# palavra reservada " global"
+num = 1
+nome = "Joao Paulo"
+valor = '1.5'
+
+print(nome + " " +  num)
